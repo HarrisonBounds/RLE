568ef63abd8e8c2d42b43f36fff28097fc0eb604
Sharwin24 <sharwin24@gmail.com>
2025-05-01 21:20:52 -0500

    Attempting to use basic mjcf

---
+++ test_jackal_in_mujoco.py
@@ -0,0 +1,25 @@
+import gymnasium as gym
+from jackal_env import Jackal_Env
+import time
+import mujoco
+
+env = Jackal_Env(
+    xml_file="jackal.xml",
+    render_mode="human")
+
+env.render()
+
+# observation = env.reset()
+# for _ in range(1000):
+#     action = env.action_space.sample()  # Take a random action
+#     observation, reward, terminated, truncated = env.step(action)
+
+#     env.render()
+
+#     # print(f"Observation: {observation.shape}, Reward: {reward}, Terminated: {terminated}, Truncated: {truncated}")
+
+#     time.sleep(0.01)
+#     if terminated or truncated:
+#         observation = env.reset()
+
+# env.close()

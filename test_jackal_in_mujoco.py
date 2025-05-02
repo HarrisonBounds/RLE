f41ad02cb433a9a8ecdcd5056e1bd134a0bf8126
Sharwin24 <sharwin24@gmail.com>
2025-05-02 15:33:25 -0500

    Finished sim test

--- test_jackal_in_mujoco.py
+++ test_jackal_in_mujoco.py
@@ -12,6 +12,19 @@ TOTAL_TIME = 10  # seconds
 STEP_TIME = 0.01  # seconds
 ITERS_FROM_TIME = int(TOTAL_TIME / STEP_TIME)
 
+
+def time_to_iters(seconds: float) -> int:
+    return int(seconds / STEP_TIME)
+
+
+# wait 5 seconds so I can start recording :)
+for _ in range(time_to_iters(5)):
+    observation = env.reset()
+    action = [0, 0, 0, 0]  # All wheels stopped
+    observation, reward, terminated, truncated = env.step(action)
+    env.render()
+    time.sleep(0.01)
+
 # Drive forward for a few seconds
 observation = env.reset()
 for _ in range(ITERS_FROM_TIME):
